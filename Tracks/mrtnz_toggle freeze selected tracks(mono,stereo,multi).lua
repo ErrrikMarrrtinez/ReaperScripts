0aae8796498807660e53d7144812961a8a9abe09
ErrrikMarrrtinez <nedprite4@gmail.com>
2023-11-19 13:35:24 +0600

    update

--- Tracks/mrtnz_toggle freeze selected tracks(mono,stereo,multi).lua
+++ Tracks/mrtnz_toggle freeze selected tracks(mono,stereo,multi).lua
@@ -1,6 +1,6 @@
 -- @description Toggle freeze selected tracks(mono, stereo, multi)
 -- @author mrtnz
--- @version 1.0
+-- @version 1.1
 
 function toggle_freeze_track()
     local selected_track = reaper.GetSelectedTrack(0, 0)
@@ -11,11 +11,11 @@ function toggle_freeze_track()
         else
             local menu = "Stereo|Mono|Multichannel"
             local choice = gfx.showmenu(menu)
-            if choice == 2 then
+            if choice == 1 then
                 reaper.Main_OnCommand(41223, 0) -- Freeze to stereo
-            elseif choice == 3 then
+            elseif choice == 2 then
                 reaper.Main_OnCommand(40901, 0) -- Freeze to mono
-            elseif choice == 4 then
+            elseif choice == 3 then
                 reaper.Main_OnCommand(40877, 0) -- Freeze to multichannel
             end
         end
