64dccab08e69faf5447a27856b32b6b757c976df
ErrrikMarrrtinez <nepdite4@gmail.com>
2023-12-14 17:57:22 +0600

    update4

--- MIDI Editor/mrtnz_MIDI chopper.lua
+++ MIDI Editor/mrtnz_MIDI chopper.lua
@@ -1,6 +1,6 @@
 -- @description MIDI Chopper v1.212
 -- @author mrtnz
--- @version 1.3
+-- @version 1.4
 -- @about
 --  This is a GUI script based on the library of the wonderful cfillion - reaimgui, 
 --  designed for midi editor, which allows you to dynamically split (having previously 
@@ -8,7 +8,7 @@
 --  slider beyond 64 values, and everything below 64 splits the note into grid values 
 --                                (1\4, 1\8, etc.).
 -- @changelog
---   - Removed everything font-related as a temporary solution to font errors on different systems. 
+--   Removed everything font-related as a temporary solution to font errors on different systems. 
 
 
 
