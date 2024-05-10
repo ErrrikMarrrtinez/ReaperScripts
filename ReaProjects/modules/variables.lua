6d96eefe053a18b420e84d9bb82584e41069e5c8
ErrrikMarrrtinez <nepdite4@gmail.com>
2024-05-10 14:36:52 +0500

    master

--- ReaProjects/modules/variables.lua
+++ ReaProjects/modules/variables.lua
@@ -29,12 +29,12 @@ MAIN_PARAMS = {
     key_touchscroll    = "ctrl+shift"
 }
 
-round_rect_window     = MAIN_PARAMS.def_round_rect_win
-round_rect_list       = MAIN_PARAMS.other_roundrect_wd
-GLOBAL_ANIMATE        = MAIN_PARAMS.global_animate
-KEY_FOR_TOUCHSCROLL   = MAIN_PARAMS.key_touchscroll
+round_rect_window      = MAIN_PARAMS.def_round_rect_win
+round_rect_list        = MAIN_PARAMS.other_roundrect_wd
+GLOBAL_ANIMATE         = MAIN_PARAMS.global_animate
+KEY_FOR_TOUCHSCROLL    = MAIN_PARAMS.key_touchscroll
 
-max_visible           = MAIN_PARAMS.max_visible_proj
+max_visible            = MAIN_PARAMS.max_visible_proj
 
 deadline_warning_color = MAIN_PARAMS.dedline_warm_col
 elevation_warning      = MAIN_PARAMS.elevation_warming
@@ -43,12 +43,35 @@ DEF_IMG_W              = MAIN_PARAMS.default_img_w
 DEF_IMG_H              = MAIN_PARAMS.default_img_h
 
 
-INDIVIDUAL_path       = MAIN_PARAMS.individual_path
-CURRENT_media_path    = MAIN_PARAMS.current_media_path
-GENERAL_media_path    = MAIN_PARAMS.general_media_path
-INDIVIDUAL_media_path = MAIN_PARAMS.individ_media_path
+INDIVIDUAL_path        = MAIN_PARAMS.individual_path
+CURRENT_media_path     = MAIN_PARAMS.current_media_path
+GENERAL_media_path     = MAIN_PARAMS.general_media_path
+INDIVIDUAL_media_path  = MAIN_PARAMS.individ_media_path
 
 
-CUSTOM_IMAGE_local    = 'customImages' .. sep
-CUSTOM_IMAGE_global   = cur_path .. CUSTOM_IMAGE_local
-TYPE_module           = MAIN_PARAMS.last_type_opened
+CUSTOM_IMAGE_local     = 'customImages' .. sep
+CUSTOM_IMAGE_global    = cur_path .. CUSTOM_IMAGE_local
+TYPE_module            = MAIN_PARAMS.last_type_opened
+
+
+COL0  = "#1a1a1a" -- main bg col \ def_bg_color
+--list and header
+COL1  = "transparent" --#5a5a5a" -- rect_bg_heading border\all border list \ #5a5a5a
+COL2  = "#4a4a4a" -- bg heading  \ 4a4a4a
+COL3  = "#2a2a2a" -- bg list \ 2a2a2a
+--popup
+COL4  = COL3..50 -- shadow bg
+COL5  = COL1..75-- border \hex_darker(COL0, -1.2)
+COL6  = COL1 -- bg \hex_darker(COL0, -0.7)
+--ENTRY
+COL7  = "#6a6a6a" -- border \6a6a6a
+COL8  = "#2a2a2a" -- bg \3a3a3a
+--ELEMENTS
+COL9  = "#3a3a3a" -- odd \3a3a3a
+COL10 = "#323232" --even \323232
+
+COL11 = "#6a6a6a" -- mouseenter \6a6a6a
+COl12 = "#9a9a9a" -- selected \9a9a9a
+
+COL13 = COL0 .. 50 -- def pad color\ 
+COL18 = "#3a3a3a"
