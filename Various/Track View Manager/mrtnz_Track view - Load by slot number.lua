eea2839d425cce655af3a150ce3f839cf6b6a02e
ErrrikMarrrtinez <nepdite4@gmail.com>
2024-09-23 21:21:07 +0500

    Update version

--- Various/Track View Manager/mrtnz_Track view - Load by slot number.lua
+++ Various/Track View Manager/mrtnz_Track view - Load by slot number.lua
@@ -1,18 +1,15 @@
 -- @noindex
 
 
-local r = reaper ; function print(...) local t = {...} for i = 1, select('#', ...) do t[i] = tostring(t[i]) end r.ShowConsoleMsg(table.concat(t, '\t') .. '\n') end
-
-
+local r = reaper
+function print(...) local t = {...} for i = 1, select('#', ...) do t[i] = tostring(t[i]) end r.ShowConsoleMsg(table.concat(t, '\t') .. '\n') end
 package.path = package.path .. ";" .. string.match(({r.get_action_context()})[2], "(.-)([^\\/]-%.?([^%.\\/]*))$") .. "?.lua"
-
 require "json"
 require "rtk"
 local f = require "functions"
 local q = rtk.quit
 
 local x, y = r.GetMousePosition()
-
 local wnd = rtk.Window{
     opacity = 0.975,
     resizable = false,
@@ -24,39 +21,90 @@ local wnd = rtk.Window{
     x = x - 14,
     y = y - 16,
 }
-
-
 local fills = {fillw = true, fillh = true}
 local container = wnd:add(rtk.VBox{}, fills)
 f.createHeader(container)
 local inputContainer = container:add(rtk.Container{autofocus=true})
 local header = inputContainer:add(rtk.Heading{wrap=true, '', cell=fills, halign='center', fontsize=100})
 
-inputContainer.onkeypress = function(self, event) 
-    local char = event.char
-    if event.keycode == rtk.keycodes.BACKSPACE then
-        header:attr('text', header.text:sub(1, -2))
-    elseif event.keycode == rtk.keycodes.ENTER then
-        local value = tonumber(header.text)
-        if value then
-            local er = f.restoreVisibleTracksSnapshot(value)
-            if er ~= nil then
-                header:attr('fontsize', 35)
-                header:attr('valign', 'center')
-                header:attr('text', er)
-                inputContainer.onkeypress = function(self, event)
-                    if event.keycode == rtk.keycodes.ENTER then
-                        q()
-                    end
+local lastBackspaceTime = 0
+local backspaceCount = 0
+
+local function processInput(input)
+    local selected_slots = {}
+    for value in input:gmatch("[^,%.]+") do
+        value = value:match("^%s*(.-)%s*$") -- Trim whitespace
+        if value:find("-") then
+            local start, stop = value:match("(%d+)-(%d+)")
+            start, stop = tonumber(start), tonumber(stop)
+            if start and stop then
+                for i = start, stop do
+                    selected_slots[i] = true
                 end
-                rtk.callafter(0.9, q)
-            else
-                q()
+            end
+        else
+            local num = tonumber(value)
+            if num then
+                selected_slots[num] = true
             end
         end
-    elseif f.isInteger(char) then
+    end
+    return selected_slots
+end
+
+inputContainer.onkeypress = function(self, event)
+    local char = event.char
+    local currentTime = r.time_precise()
+    if event.keycode == rtk.keycodes.BACKSPACE then
+        if header.text == "" then
+            if currentTime - lastBackspaceTime < 0.3 then
+                backspaceCount = backspaceCount + 1
+                if backspaceCount == 2 then
+                    f.setAllVisibleTracks()
+                    rtk.callafter(0.3, q)
+                    return true
+                end
+            else
+                backspaceCount = 1
+            end
+            lastBackspaceTime = currentTime
+        else
+            header:attr('text', header.text:sub(1, -2))
+        end
+    elseif event.keycode == rtk.keycodes.ENTER then
+        local input = header.text
+        if input:find("[,%.%-]") then
+            local selected_slots = processInput(input)
+            if next(selected_slots) then
+                f.main_loader(selected_slots)
+                q()
+            else
+                header:attr('fontsize', 35)
+                header:attr('valign', 'center')
+                header:attr('text', "Invalid input")
+                rtk.callafter(0.9, q)
+            end
+        else
+            local value = tonumber(input)
+            if value then
+                local er = f.restoreVisibleTracksSnapshot(value)
+                if er ~= nil then
+                    header:attr('fontsize', 35)
+                    header:attr('valign', 'center')
+                    header:attr('text', er)
+                    inputContainer.onkeypress = function(self, event)
+                        if event.keycode == rtk.keycodes.ENTER then
+                            q()
+                        end
+                    end
+                    rtk.callafter(0.9, q)
+                else
+                    q()
+                end
+            end
+        end
+    elseif f.isInteger(char) or char == ',' or char == '.' or char == '-' then
         header:attr('text', header.text .. char)
-        
     elseif event.keycode == rtk.keycodes.ESCAPE then
         q()
     end
@@ -67,11 +115,11 @@ wnd.onupdate = function()
     inputContainer:focus()
 end
 
-wnd.onfocus=function()
+wnd.onfocus = function()
     return true
 end
 
-wnd.onblur=function()
+wnd.onblur = function()
     rtk.callafter(0.3, q)
 end
 
