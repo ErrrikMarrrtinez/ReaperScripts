45af65caafcc8c8b1230e47a0a1a2b8004775d28
ErrrikMarrrtinez <nepdite4@gmail.com>
2025-02-23 10:40:29 +0500

    check new

--- Various/anibla/mrtnz_scripts-anibla.lua
+++ Various/anibla/mrtnz_scripts-anibla.lua
@@ -1,6 +1,6 @@
 -- @description Anibla scripts
 -- @author mrtnz
--- @version 0.3.7
+-- @version 0.3.8
 -- @provides
 --   [main] .
 --   [main] mrtnz_insert text item by region under mouse.lua
@@ -8,6 +8,7 @@
 --   [main] mrtnz_Load subprojects in current project.lua
 --   [main] mrtnz_insert update regions.lua
 --   [main] mrtnz_subtitles manager.lua
+--   [main] mrtnz_toggle show grid line.lua
 --   Reateam_RPP-Parser.lua
 --   mrtnz_srtass-parser.lua
 --   mrtnz_utils.lua
