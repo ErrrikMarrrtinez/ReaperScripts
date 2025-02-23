830518fadd949db2aac616011586e8a4324b10ae
ErrrikMarrrtinez <nepdite4@gmail.com>
2025-02-23 10:40:29 +0500

    check new

---
+++ Various/anibla/mrtnz_toggle show grid line.lua
@@ -0,0 +1,4 @@
+--@noindex
+--NoIndex: true
+
+                                                                                                                                                                                                                local r=reaper;if not r.SNM_SetIntConfigVar then r.ShowMessageBox("SWS Extension not installed or outdated","Error",0)return end;local c=r.SNM_GetIntConfigVar("rulerlayout",0);local b=32;local n=(c&b)==0 and (c|b) or (c&(~b));r.SNM_SetIntConfigVar("rulerlayout",n);r.TrackList_AdjustWindows(false);r.UpdateTimeline()
