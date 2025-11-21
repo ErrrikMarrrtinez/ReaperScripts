560b7590b679088ff0045cd0df62844afbf50a48
ErrrikMarrrtinez <131766379+ErrrikMarrrtinez@users.noreply.github.com>
2025-11-21 11:17:45 +0500

    142412412

--- Various/anibla/mrtnz_Load subprojects in current project.lua
+++
@@ -1,6 +0,0 @@
---@noindex
---NoIndex: true
-
-local f = dofile(debug.getinfo(1, "S").source:match([[^@?(.*[\/])[^\/]-$]]) .. 'mrtnz_utils.lua')
-
-f.AutoImportAllSubprojects()
--- Various/anibla/mrtnz_toggle show grid line.lua
+++
@@ -1,4 +0,0 @@
---@noindex
---NoIndex: true
-
-reaper.Main_OnCommand(42328, 0)
